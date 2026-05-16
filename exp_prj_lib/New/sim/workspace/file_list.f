1576cb85e9cb8daf3b840131aed7bea69dfb8cac
Kevin Jin <1912247288@qq.com>
2026-05-16 20:12:41 +0800

    feat: Added the path to the include directory to the modelsim script

--- exp_prj_lib/New/sim/workspace/file_list.f
+++ exp_prj_lib/New/sim/workspace/file_list.f
@@ -1,4 +1,11 @@
-../../rtl/*.sv
+// include指令搜索路径
++incdir+../../rtl/include
++incdir+../../tb/include
+
+// 设计源码搜索路径
+// ../../rtl/*.sv
 ../../rtl/*.v
-../../tb/*.sv
+
+// 测试源码搜索路径
+// ../../tb/*.sv
 ../../tb/*.v
